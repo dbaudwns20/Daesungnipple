f9532811d7947350de561a7965ec842c8e22ec88
Junim <dbaudwns20@gmail.com>
2024-09-03 01:17:56 +0900

    OAuth Provider 컴포넌트 추가, Naver Oauth 인터셉터 추가

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,26 @@
-import { handlers } from "@/auth"; // Referring to the auth.ts we just created
-export const { GET, POST } = handlers;
+import { type NextRequest, NextResponse } from "next/server";
+
+import { handlers, naverFetchInterceptor } from "@/auth"; // Referring to the auth.ts we just created
+
+const AuthGET = handlers.GET;
+const AuthPOST = handlers.POST;
+
+const originalFetch = fetch;
+
+export async function GET(req: NextRequest) {
+  const url: URL = new URL(req.url);
+
+  // 네이버 OAuth2 는 OAuth 2.0 스펙을 엄격히 지키지 않기 때문에 인터셉터로 중간 처리 필요
+  if (url.pathname === "/api/auth/callback/naver") {
+    global.fetch = naverFetchInterceptor(originalFetch);
+    const response = await AuthGET(req);
+    global.fetch = originalFetch;
+    return response;
+  }
+
+  return await AuthGET(req);
+}
+
+export async function POST(req: NextRequest) {
+  return await AuthPOST(req);
+}
--- src/auth.ts
+++ src/auth.ts
@@ -1,41 +1,88 @@
 import NextAuth from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/prisma";
-
-import Credentials from "next-auth/providers/credentials";
-
 import Google from "next-auth/providers/google";
+import Kakao from "next-auth/providers/kakao";
+import Naver from "next-auth/providers/naver";
+import Credentials from "next-auth/providers/credentials";
+import type { Provider } from "next-auth/providers";
+
+import { getAuthUser, checkUserExist } from "@/services/auth/sign.in.service";
+
+const providers: Provider[] = [
+  Credentials({
+    credentials: {
+      email: { label: "Email", type: "email" },
+      password: { label: "Password", type: "password" },
+    },
+    authorize: async (credentials) => {
+      let user: any = await getAuthUser({
+        email: credentials.email as string,
+        password: credentials.password as string,
+      });
+      return user;
+    },
+  }),
+  Google,
+  Naver,
+  Kakao,
+];
+
+/**
+ * 네이버 OAuth2 는 OAuth 2.0 스펙을 엄격히 지키지 않기 때문에 인터셉터로 중간 처리 필요
+ * expires_in 이 string type 으로 응답을 받기 때문에 Auth.js 에서 에러발생.
+ * response 를 새로 만들어서 리턴한다.
+ * @param originalFetch
+ * @returns
+ */
+export const naverFetchInterceptor =
+  (originalFetch: typeof fetch) =>
+  async (
+    url: Parameters<typeof fetch>[0],
+    options: Parameters<typeof fetch>[1] = {},
+  ) => {
+    // 네이버 토큰 호출 API 만 인터셉트 한다
+    if (
+      url === "https://nid.naver.com/oauth2.0/token" &&
+      options.method === "POST"
+    ) {
+      const response = await originalFetch(url, options);
+      /* Clone the response to be able to modify it */
+      const clonedResponse = response.clone();
+      const body = await clonedResponse.json();
+
+      body.expires_in = Number(body.expires_in);
+
+      /*  Create a new response with the modified body */
+      const modifiedResponse = new Response(JSON.stringify(body), {
+        status: response.status,
+        statusText: response.statusText,
+        headers: response.headers,
+      });
+
+      /* Add the original url to the response */
+      return Object.defineProperty(modifiedResponse, "url", {
+        value: response.url,
+      });
+    }
+
+    return originalFetch(url, options);
+  };
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    Credentials({
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      credentials: {
-        email: {},
-        password: {},
-      },
-      authorize: async (credentials) => {
-        let user = null;
-
-        // logic to salt and hash password
-        // const pwHash = saltAndHashPassword(credentials.password);
-
-        // logic to verify if the user exists
-        // user = await getUserFromDb(credentials.email, pwHash);
-
-        if (!user) {
-          // No user found, so this is their first attempt to login
-          // meaning this is also the place you could do registration
-          throw new Error("User not found.");
-        }
-
-        // return user object with their profile data
-        return user;
-      },
-    }),
-
-    Google,
-  ],
+  providers: providers,
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email, name } = user;
+      const provider: string = account!.provider;
+      // 가입된 이용자가 아니라면 회원가입 페이지로 이동시킨다
+      if (await checkUserExist(user.email!)) {
+        return encodeURI(
+          `/sign-up?email=${email}&name=${name}&provider=${provider.toUpperCase()}`,
+        );
+      }
+      return true;
+    },
+  },
+  pages: {
+    signIn: "/sign-in", // 커스텀 페이지
+  },
 });
