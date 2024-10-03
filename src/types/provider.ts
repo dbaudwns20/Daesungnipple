8dca86d55c233734b43975e5eb2acd5057ac0bad
Junim <dbaudwns20@gmail.com>
2024-10-03 16:40:43 +0900

    enum 제거 TreeShaking 이슈로 인해 Union Type 으로 변경

--- src/types/provider.ts
+++
@@ -1,9 +0,0 @@
-export enum Provider {
-  GOOGLE = "GOOGLE",
-  KAKAO = "KAKAO",
-  NAVER = "NAVER",
-}
-
-export function getValue(provider: string) {
-  return Provider[provider as keyof typeof Provider];
-}
