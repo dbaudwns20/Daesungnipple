1d93a9730c3cdc1d0442449d482213d384262a87
Junim <dbaudwns20@gmail.com>
2024-10-08 17:42:20 +0900

    rename product to product.service

--- src/actions/product.actions.ts
+++ src/actions/product.actions.ts
@@ -1,20 +1,20 @@
 "use server";
 
 import type { Product } from "@/types/product";
-import { createProduct } from "@/services/product";
+import { createProduct } from "@/services/product.service";
 
 type ProductActionResponse = {
   ok: boolean;
   message: string;
   data?: any;
-}
+};
 
 export async function CreateProductAction(formData: FormData) {
   let { ok, message } = { ok: true, message: "상품이 등록되었습니다." };
   try {
     const productJSON = formData.get("product") as string;
     const productObj = JSON.parse(productJSON) as Product;
-    console.log(productObj)
+    console.log(productObj);
     await createProduct(productObj);
   } catch (e: any) {
     ok = false;
@@ -22,6 +22,6 @@ export async function CreateProductAction(formData: FormData) {
   }
   return {
     ok,
-    message
+    message,
   } as ProductActionResponse;
 }
--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -3,16 +3,18 @@
 import { NextRequest, NextResponse } from "next/server";
 
 import type { ProductListOption } from "@/types/product";
-import { listProduct } from "@/services/product";
+import { listProduct } from "@/services/product.service";
 import { delay } from "@/utils/common";
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const listOption: ProductListOption = {
     name: searchParams.get("name") || "",
-    categoryId: searchParams.get("categoryId") ? parseInt(searchParams.get("categoryId") || "0", 10) : null,
+    categoryId: searchParams.get("categoryId")
+      ? parseInt(searchParams.get("categoryId") || "0", 10)
+      : null,
     page: parseInt(searchParams.get("page") || "1", 10), // 기본값 1
-    unit: parseInt(searchParams.get("unit") || "10", 10) // 기본값 10
+    unit: parseInt(searchParams.get("unit") || "10", 10), // 기본값 10
   };
 
   // TODO isLoading 테스트
