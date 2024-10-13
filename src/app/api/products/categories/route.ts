b7ecf16a8fa2f25a191316839a63f9295fc89197
Junim <dbaudwns20@gmail.com>
2024-10-13 15:58:48 +0900

    /management 경로 수정 (shop) 레이아웃과 분리

--- src/app/api/products/categories/route.ts
+++ src/app/api/products/categories/route.ts
@@ -1,25 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import { ProductCategory } from "@prisma/client";
+import { type ProductCategoryForAdmin } from "@/types/product.category";
 
 import { getCategoryList } from "@/services/category.service";
 
 export async function GET(request: NextRequest) {
-  let list: ProductCategory[] = [];
+  let list: ProductCategoryForAdmin[] = [];
   try {
     // TODO 토큰이나 인증 값 같은 걸로 호출자가 어드민인지 확인 필요
     const isForAdmin = true;
 
     list = await getCategoryList(isForAdmin);
-  } catch (e: any) {
-    return NextResponse.json({ message: e.message }, { status: 500 });
-  } finally {
     return NextResponse.json(
       {
-        message: "retrieved",
+        message: "조회되었습니다.",
         list,
       },
       { status: 200 },
     );
+  } catch (e: any) {
+    return NextResponse.json({ message: e.message }, { status: 500 });
   }
 }
---
+++ src/app/management/categories/page.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { useEffect, useRef, useCallback } from "react";
+
+import Grid from "@/components/grid";
+import { type GridOptions, type GridType } from "@/components/grid/type";
+import Button, { type ButtonType } from "@/components/button/button";
+
+import { showToast } from "@/utils/message";
+
+export default function CategoriesPage() {
+  const gridRef = useRef<GridType>();
+  const buttonRef = useRef<ButtonType>(null);
+
+  const gridOptions: GridOptions = {
+    scrollX: false,
+    rowHeaders: ["checkbox"],
+    draggable: true,
+    treeColumnOptions: {
+      name: "name",
+      useCascadingCheckbox: true,
+    },
+    columns: [
+      {
+        header: "이름",
+        name: "name",
+      },
+      {
+        header: "사용여부",
+        name: "isActive",
+        width: 200,
+      },
+    ],
+    contextMenu: null,
+  };
+
+  const handleButtonClick = (p: "umount" | "expandAll" | "collapseAll") => {
+    switch (p) {
+      case "umount":
+        gridRef.current?.unmount();
+        break;
+      case "expandAll":
+        gridRef.current?.expandAll();
+        break;
+      case "collapseAll":
+        gridRef.current?.collapseAll();
+        break;
+    }
+  };
+
+  const getCategoryList = useCallback(async () => {
+    const res: Response = await fetch("/api/products/categories");
+    const data = await res.json();
+    if (!res.ok) showToast({ message: data.message });
+    else gridRef.current!.setGridData(data.list);
+  }, []);
+
+  useEffect(() => {
+    getCategoryList();
+  }, [getCategoryList]);
+
+  return (
+    <>
+      <header className="mb-1.5 flex items-center justify-between">
+        <h1 className="font-bold text-gray-600">카테고리 관리</h1>
+        <div className="flex gap-1">
+          <Button
+            ref={buttonRef}
+            type="button"
+            color="blue"
+            size="xs"
+            onClick={() => handleButtonClick("umount")}
+          >
+            그리드 제거
+          </Button>
+          <Button
+            ref={buttonRef}
+            type="button"
+            color="blue"
+            size="xs"
+            onClick={() => handleButtonClick("expandAll")}
+          >
+            모든 행 열기
+          </Button>
+          <Button
+            ref={buttonRef}
+            type="button"
+            color="blue"
+            size="xs"
+            onClick={() => handleButtonClick("collapseAll")}
+          >
+            모든 행 닫기
+          </Button>
+        </div>
+      </header>
+
+      <Grid ref={gridRef} gridOptions={gridOptions} />
+    </>
+  );
+}
