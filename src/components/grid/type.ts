c9b70ba10f8a4900aabb74bd239dcc75e8223585
Junim <dbaudwns20@gmail.com>
2024-10-19 14:24:59 +0900

    change interface to type

--- src/components/grid/type.ts
+++ src/components/grid/type.ts
@@ -1,17 +1,17 @@
 import { type OptGrid } from "tui-grid/types/options";
 
-export interface GridOptions extends Omit<OptGrid, "el"> {
+export type GridOptions = {
   el?: HTMLElement; // el 은 컴포넌트 내에서 선언되기 때문에 Option 으로 변경
-}
+} & Omit<OptGrid, "el">;
 
-export interface GridProps {
+export type GridProps = {
   gridOptions: GridOptions;
-}
+};
 
-export interface GridType {
+export type GridType = {
   setGridData: (newData: any) => void;
   unmount: () => void;
   expandAll: () => void;
   collapseAll: () => void;
   element: HTMLDivElement | null;
-}
+};
